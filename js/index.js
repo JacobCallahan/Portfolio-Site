e671cd286db1c9213d81d0327dfe61e48d09dc72
JacobCallahan <jacob.callahan05@gmail.com>
2014-08-04 23:19:36 -0400

    cleaning up
    Cleaning up some testing things. Mainly the now useless button, and the
    associated styles and code.

--- js/index.js
+++ js/index.js
@@ -1,14 +1,3 @@
-function testSlider(target) {
-	//simple function that will toggle the notifications
-	$element = $('.' + target);
-    $element.attr("slide","yep!");
-    setTimeout(function() {
-    	$element.hide();
-    	$element.attr("slide","");
-    	$element.show();
-    },1000); //set the timeout equal to your desired animation length
-}
-
 $(document).ready(function () {
 	$('#WebDev').click(function () {
 		$('#Software').removeClass("selected");
