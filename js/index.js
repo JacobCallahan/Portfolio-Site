f0c31368cef76b07abc1b230a4494f75b10b69e2
JacobCallahan <jacob.callahan05@gmail.com>
2014-08-04 23:39:50 -0400

    More cosmetic changes
    some simple styling changes as well as changing the slide function to a
    more descriptive name.

--- js/index.js
+++ js/index.js
@@ -1,10 +1,23 @@
+function slideContentDown(target) {
+	//simple function that will toggle the notifications
+	$element = $('.' + target);
+    $element.attr("slide","yep!");
+    setTimeout(function() {
+    	$element.hide();
+    	$element.attr("slide","");
+    	$element.show();
+    },1000); //set the timeout equal to your desired animation length
+}
+
 $(document).ready(function () {
 	$('#WebDev').click(function () {
+		//todo: add code to switch out the content
 		$('#Software').removeClass("selected");
-		testSlider('showcase');
+		slideContentDown('showcase');
 		$(this).addClass("selected");
 	});
-	$('img').click(function () {		
+	$('img').click(function () {	
+		//simple modular popup that switches the image source and unhides the mod	
 		$('.modImg').attr("src", $(this).attr("src"));
 		$('.modPop').removeClass('hideMe');
 		$('.modFrame').removeClass('hideMe');
